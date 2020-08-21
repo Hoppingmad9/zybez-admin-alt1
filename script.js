2c495fba46f42618ff4bf8ac3f7ad5dfb42a9f29
Sam Dean <samdena09@gmail.com>
2020-08-21 21:06:17 +0100

    chat box testing

---
+++ script.js
@@ -0,0 +1,43 @@
+a1lib.idtentifyUrl("appconfig.json");
+var reader = new ChatBoxReader();
+reader.readargs = {
+    colors: [
+        a1lib.mixcolor(255,255,255),
+    ],
+    backwards: true,
+};
+reader.find();
+
+if (reader.pos === null) {
+    $(".mats").append(
+        "<span>Chat not detected. PLeaser ensure that you are in-game, then click <button onclick='location.reload()'>Refresh</button></span>"
+    );
+} else {
+    reader.pos.mainbox = reader.pos.boxes[0];
+}
+showSelectedChat(reader.pos);
+
+var chatCheck = reader.read();
+var index;
+
+function showSelectedChat(chat) {
+    //Attempt to show a temporary rectangle around the chatbox.  skip if overlay is not enabled.
+    try {
+      alt1.overLayRect(
+        a1lib.mixcolor(255, 255, 255),
+        chat.mainbox.rect.x,
+        chat.mainbox.rect.y,
+        chat.mainbox.rect.width,
+        chat.mainbox.rect.height,
+        2000,
+        1
+      );
+    } catch { }
+}
+
+function readChatbox() {
+    var opts = reader.read() || [];
+    var chat = "";
+
+    console.log(opts);
+}
