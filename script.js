2ff32683990e710ead1d1d9ca6fe481f84c76f7d
Sam Dean <samdena09@gmail.com>
2020-08-22 01:41:29 +0100

    updated test data

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function readChatbox() {
             console.log(username);
         }
         if (item.indexOf("Provides easy access to your bank") > -1) {
-            let username = 'Ironmad9';
+            let username = 'Test man. Didn\'t actually cap.';
             window.open(
                 'https://zybez.co.uk/data_submissions/receive_citadel_capping_usernames.php?username='+username,
                 'myIframe'
